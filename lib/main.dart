08ce8a3eeafc44f9c635210f9ce82b21fb99c364
ash Fahim <ashfahim18@gmail.com>
2023-11-20 22:16:07 +0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -68,6 +68,24 @@ class Homescreen extends StatelessWidget{
                   );
                 },
                 child: Text('Go to settings')
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                  onPressed: (){
+                    ///Navigation
+                    ///Route Homescreen to Route Setting Screen
+                    ///Navigation (push,pop,replace,replaceAll,removeUntil
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder:(context)=>ProductListScreen()
+                        )
+                    );
+                  },
+                  child: Text('Go to ProductList')
+              ),
             )
           ],
         ),
@@ -99,36 +117,101 @@ class SettingScreen extends StatelessWidget{
   }
 }
 ///Route
-class OrderScreen extends StatelessWidget{
+class OrderScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Order'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Order',style: TextStyle(
-                fontSize: 30
-            ),
-            ),
-            ElevatedButton(onPressed:(){
-
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => SettingScreen()
-                  )
-              );
-            } ,
-                child:Text('Go To Settings')
-            )
-          ],
+        appBar: AppBar(
+          title: Text('Order'),
+          centerTitle: true,
         ),
-      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Order', style: TextStyle(
+                  fontSize: 30
+              ),
+              ),
+              ElevatedButton(onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => SettingScreen()
+                    )
+                );
+              },
+                  child: Text('Go To Settings')
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
+class ProductListScreen extends StatelessWidget {
+  const ProductListScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Product List'),
+          centerTitle: true,
+        ),
+        body:ListView.builder(
+            itemCount: 20,
+            itemBuilder: (context,index) {
+              return ListTile(
+                onTap: (){
+                  Navigator.push(context,MaterialPageRoute(builder: (context)=> ProductDetailsScreen(ProductName: '$index', Price:(index*100.0)))
+
+                  ).then((value){
+                    print(value);
+                  } ///(.then) => To Back from front screen with a value.
+                  );
+                },
+                title: Text('$index'),
+              );
+            }
+        )
+    );
+  }
+}
+
+class ProductDetailsScreen extends StatelessWidget {
+  final String ProductName;
+  final double? Price;
+  const ProductDetailsScreen({super.key,required this.ProductName, this.Price});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Product Details'),
+          centerTitle: true,
+        ),
+        body:Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Product Name: $ProductName',style: TextStyle(
+                  fontSize: 25
+              ),
+              ),
+              Text('Product Price: $Price',style: TextStyle(
+                  fontSize: 25
+              ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(onPressed: (){
+                  Navigator.pop(context,"Back from Product $ProductName");
+                },
+                    child: Text('Go Back')
+                ),
+              )
+            ],
+          ),
+        )
     );
   }
 }
