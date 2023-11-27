03d4e708d844991b36811d3c4a9bd752fb99a388
ash Fahim <ashfahim18@gmail.com>
2023-11-27 21:41:44 +0600

    Dialog and bottom sheet Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main(){
   runApp(
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home:Home(),
     );
@@ -18,40 +19,67 @@ class MyApp extends StatelessWidget {
 }
 class Home extends StatelessWidget {
   const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        leading:Icon(CupertinoIcons.home),
-        middle: Text('Home'),
-        trailing: CupertinoSwitch(
-          onChanged: (bool onchange){},value: true,
-        ),
+    return Scaffold(
+      appBar:AppBar(
+        title:Text('Home'),
+        centerTitle: true,
+        leading: Icon(Icons.home),
+        actions: [Icon(Icons.add_business_outlined)],
       ),
-        child:Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Home'),
-                Text('Home'),
-                Text('Home'),
-                Text('Home'),
-                Text('Home'),
-                CupertinoButton(child: Text('Tap'), onPressed:(){}),
-                CupertinoButton.filled(child: Text('Tap'), onPressed:(){}),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CupertinoTextField(),
+      body: Column(
+        children: [
+          Switch(value: true, onChanged:(bool value){}),
+          ElevatedButton(onPressed:(){
+            showDialog(
+              barrierDismissible: false,
+                context: context, builder:(context) {
+              return AlertDialog(
+                title: Text('Alert'),
+                content: Text('You are in Danger'),
+                actions: [
+                  TextButton(onPressed: (){
+                    Navigator.pop(context);
+                  }, child:Text('Cancel')),
+                  TextButton(onPressed: (){}, child:Text('Ok')),
+                 ],
+                );
+              }
+            );
+          } , child: Text('Show dialog')),
+          ElevatedButton(onPressed: (){
+            showAboutDialog(context: context,applicationName:"Book and Kick");
+           }, child: Text('Show about Dialog')),
+          ElevatedButton(onPressed: (){
+            showModalBottomSheet(
+              ///isDismissible: false,
+              barrierColor: Colors.black54,
+                shape:RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topRight:Radius.circular(15),
+                    topLeft: Radius.circular(15),
+                  )
                 ),
-                CupertinoTabBar(items:[
-                  BottomNavigationBarItem(label:'Home',icon:Icon(CupertinoIcons.home)),
-                  BottomNavigationBarItem(label:'Home',icon:Icon(CupertinoIcons.home)),
-                  BottomNavigationBarItem(label:'Home',icon:Icon(CupertinoIcons.home)),
-                ])
-              ],
-            )
-        ),
+                context:context, builder:(context){
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Argentina 1-0 Brazil'),
+                  ],
+                ),
+              );
+             }
+            );
+          },
+              child:Text("Show Sheet")
+          )
+         ],
+      ),
+
     );
   }
 }
-
